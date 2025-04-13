222477a78ebf55748a0077b2d3ccefd4e20d5ba3
Botta Michele <72565868+bottamichele@users.noreply.github.com>
2025-04-13 19:19:13 +0200

    Class agent added

---
+++ dqn_algorithms/agent.py
@@ -0,0 +1,203 @@
+import os
+import copy
+import pickle
+import torch as tc
+
+from torch.optim import Adam
+from torch.nn.functional import mse_loss
+
+from dqn_algorithms.memory import UniformMemory, PropPriorMemory
+from dqn_algorithms.policy import e_greedy_policy
+from dqn_algorithms.q_methods import compute_q_dqn, compute_q_ddqn
+
+class DQNAgent:
+    """A DQN training agent."""
+
+    def __init__(self, model, mem_params, update_target_step, batch_size, lr, gamma, eps_init, eps_fin, eps_dec, device, use_double=True):
+        """Create new DQN training agent.
+        
+        Parameters
+        --------------------
+        model: DQN-like
+            a DQN model
+
+        mem_params: dict
+            memory replay parameters. The dict contains the type of memory replay and
+            its own parameters.
+
+        update_target_step: int
+            how many steps target model is updated
+            
+        batch_size: int
+            minibatch size
+
+        lr: float
+            learning rate
+
+        gamma: float
+            discount factor
+
+        eps_init: float
+            initial epsilon value
+
+        eps_fin: float
+            final epsilon value
+
+        eps_dec: float
+            epsilon decay 
+
+        device: tc.device
+            device model is trained on
+
+        use_double: bool, optional
+            whether or not to use Double Q-Learning
+        """
+
+        #Hyperparameters.
+        self.update_target_step = update_target_step
+        self.use_double = use_double
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.epsilon = eps_init
+        self.epsilon_final = eps_fin
+        self.epsilon_decay = eps_dec
+        self.device = device
+
+        #DQN model.
+        self.model = model.to(device=device)
+        self.target_model = copy.deepcopy(self.model)
+        self.optimizer = Adam(self.model.parameters(), lr=lr)
+
+        self.model.train()
+        self.target_model.eval()
+
+        #Memory replay.
+        if mem_params["type"] == "uniform_memory":
+            self.memory = UniformMemory(mem_params["mem_size"], 
+                                        mem_params["obs_size"], 
+                                        mem_params.get("obs_dtype", tc.float32), 
+                                        tc.int32, 
+                                        tc.float32, 
+                                        device=device)
+        elif mem_params["type"] == "proportional_prioritized_memory":
+            self.memory = PropPriorMemory(mem_params["mem_size"], 
+                                          mem_params["obs_size"],
+                                          alpha=mem_params.get("alpha", 0.6),
+                                          beta=mem_params.get("beta", 0.4),
+                                          eps=mem_params.get("eps", 10**-5),
+                                          obs_dtype=mem_params.get("obs_dtype", tc.float32),
+                                          act_dtype=tc.int32,
+                                          rew_dtype=tc.float32,
+                                          device=device)
+        else:
+            raise ValueError("The dict \'mem_params\' does not specify any type of memory replay.")
+
+    def choose_action(self, obs):
+        """Choose an action to perform.
+        
+        Parameter
+        --------------------
+        obs: tc.Tensor
+            an observation"""
+        
+        return e_greedy_policy(self.model, self.epsilon, obs.to(device=self.device))
+    
+    def remember(self, obs, action, reward, next_obs, next_obs_done):
+        """Remember a transiction.
+        
+        Parameters
+        --------------------
+        obs: tc.Tensor
+            an observation
+            
+        action: int
+            an action choosen from obs
+            
+        reward: float
+            reward obtained to perform action
+        
+        next_obs: tc.Tensor
+            obsevation obtained to perform action
+            
+        next_obs_done: bool
+            True if next_obs is a terminal state, False otherwise."""
+        
+        self.memory.store_transiction(obs, action, reward, next_obs, next_obs_done)
+
+    def train(self):
+        """Do a train step."""
+
+        if len(self.memory) >= self.batch_size:
+            #Sample a mini-batch.
+            if isinstance(self.memory, UniformMemory):
+                obs_b, action_b, reward_b, next_obs_b, next_obs_done_b = self.memory.sample_batch(self.batch_size)
+            else:
+                obs_b, action_b, reward_b, next_obs_b, next_obs_done_b, weight_b = self.memory.sample_batch(self.batch_size)
+
+            next_obs_done_b = next_obs_done_b.to(dtype=tc.int32)
+
+            #Computet q values.
+            if not self.use_double:
+                q, q_target = compute_q_dqn(self.model, self.gamma, obs_b, action_b, reward_b, next_obs_b, next_obs_done_b, self.target_model)
+            else:
+                q, q_target = compute_q_ddqn(self.model, self.target_model, self.gamma, obs_b, action_b, reward_b, next_obs_b, next_obs_done_b)
+
+            #Compute loss and gradient.
+            if isinstance(self.memory, UniformMemory):
+                loss = mse_loss(q, q_target).to(device=self.device)
+            else:
+                loss = tc.mean((q_target - q).pow(2) * weight_b)
+            
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            #Update priorities if memory replay is a PER.
+            if isinstance(self.memory, PropPriorMemory):
+                td_errors = tc.clamp(q_target - q, -1.0, 1.0)
+                self.memory.update_priorities(td_errors)
+                    
+        self.epsilon = self.epsilon - self.epsilon_decay if self.epsilon > self.epsilon_final else self.epsilon_final
+
+    def update_target_model(self):
+        """Update target model's parameters."""
+
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    def save_session(self, path):
+        """Save current session of this agent on disk.
+        
+        Parameter
+        --------------------
+        path: str
+            path where current session of this agent is saved on"""
+        
+        with open(os.path.join(path, "dqn_agent_session.pkl"), "wb") as session_file:
+            pickle.dump(self, session_file)
+
+    def load_session(path):
+        """Load a session of DQN agent from disk.
+        
+        Parameter
+        --------------------
+        path where a session of DQN agent is on
+        
+        Return
+        --------------------
+        agent: DQNAgent
+            a DQN training agent"""
+
+        with open(os.path.join(path, "dqn_agent_session.pkl"), "rb") as session_file:
+            agent = pickle.load(session_file)
+
+        return agent
+
+    def save_model(self, path):
+        """Save model on disk.
+        
+        Parameter
+        --------------------
+        path: str
+            path where model is saved on"""
+        
+        tc.save(self.model.state_dict(), os.path.join(path, "model.pth"))
---
+++ test/dqn_agent.py
@@ -0,0 +1,93 @@
+import gymnasium as gym
+import numpy as np
+import torch as tc
+
+from gymnasium.wrappers import NumpyToTorch
+
+from dqn_algorithms.nn import DuelingDQN
+from dqn_algorithms.agent import DQNAgent
+
+# ========================================
+# ============ HYPERPARAMETERS ===========
+# ========================================
+
+EPISODES = 500
+BATCH_SIZE = 32
+GAMMA = 0.99
+EPSILON_INIT = 1.0
+EPSILON_MIN = 0.01
+EPSILON_DECAY = 10**-4
+UPDATE_RATE = 250
+MEMORY_SIZE = 5000
+LEARNING_RATE = 10**-3
+DEVICE = tc.device("cpu")
+USE_PRIORITY_MEMORY = False
+
+# ========================================
+# =============== COSTANTS ===============
+# ========================================
+
+OBSERVATION_SIZE = 0
+ACTION_SIZE = 0
+
+# ========================================
+# ================= MAIN =================
+# ========================================
+
+if __name__ == "__main__":
+    #Create enviroment.
+    env = gym.make("CartPole-v1")
+    env = NumpyToTorch(env, device=DEVICE)
+    OBSERVATION_SIZE = env.observation_space.shape[0]
+    ACTION_SIZE = env.action_space.n
+
+    #Create model.
+    model = DuelingDQN(OBSERVATION_SIZE, ACTION_SIZE, 256, 1, 1, 1).to(DEVICE)
+
+    #Create memory replay params.
+    if not USE_PRIORITY_MEMORY:
+        type_memory = "uniform_memory"
+    else:
+        type_memory = "proportional_prioritized_memory"
+    
+    memory_params = {"type": type_memory, "mem_size": MEMORY_SIZE, "obs_size": OBSERVATION_SIZE}
+    
+    #Define DQN training agent.
+    agent = DQNAgent(model, memory_params, UPDATE_RATE, BATCH_SIZE, LEARNING_RATE, GAMMA, EPSILON_INIT, EPSILON_MIN, EPSILON_DECAY, DEVICE)
+
+    #Training phase.
+    scores = []
+    total_states = 0
+
+    for episode in range(1, EPISODES+1):
+        #Episode.
+        obs, _ = env.reset()
+        epis_done = False
+        scores.append(0)
+        
+        while not epis_done:
+            #Choose action.
+            action = agent.choose_action(obs)
+
+            #Perform action choosen.
+            next_obs, reward, terminated, truncated, _ = env.step(action)
+            epis_done = terminated or truncated
+
+            #Store current transiction.
+            agent.remember(obs, action, reward, next_obs, epis_done)
+
+            #Do train step.
+            agent.train()
+                    
+            #Updates.
+            obs = next_obs
+            scores[-1] += reward
+            total_states += 1
+
+            if total_states % agent.update_target_step == 0:
+                agent.update_target_model()
+
+        #Print training stats of current epidode ended.
+        print("- Episode {:3d}: score = {:3.0f}; avg score = {:3.2f}; total states = {:>5d}; epsilon = {:.2f}".format(episode, scores[-1], np.mean(scores[-100:]), total_states, agent.epsilon))
+
+    env.close()
